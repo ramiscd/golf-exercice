09601a7f97274c6f0ddb40487c9806d1cd46d52d
Ramis Damasceno <ramis1998d@gmail.com>
2023-09-30 11:54:19 -0300

    manipulating-complex-objects

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,22 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Ramis",
+    "title": "1 2 3 4",
+    "release_year": 2023,
+    "formats": [
+      "CD",
+      "DVD"
+    ],
+  }
+];
